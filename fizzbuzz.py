1cef1df7a732e60ed2fd0ea26f172206cbc64754
Rogerio Francisco de Aguiar <rogerio.aguiar@dextra-sw.com>
2018-06-13 14:14:50 -0300

    M2A08: Por dentro do framework de testes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -26,21 +26,3 @@ def robot(pos):
         say = 'fizz'
 
     return say
-
-
-if __name__ == '__main__':
-    assert robot(1) == '1'
-    assert robot(2) == '2'
-    assert robot(4) == '4'
-
-    assert robot(3) == 'fizz'
-    assert robot(6) == 'fizz'
-    assert robot(9) == 'fizz'
-
-    assert robot(5) == 'buzz'
-    assert robot(10) == 'buzz'
-    assert robot(20) == 'buzz'
-
-    assert robot(15) == 'fizzbuzz'
-    assert robot(30) == 'fizzbuzz'
-    assert robot(45) == 'fizzbuzz'
